b04fcbb81d4393e9dc27012677c89774697562f1
benlukka <68825026+benlukka@users.noreply.github.com>
2025-05-26 02:24:33 +0200

    (WIP) initial
    The Code generation was improved. the whole setup was edited to be for an api specification that is dynamic and not static from the spc.jon file for the Openapi code gen. Also added a function that automatically creates a json response format for the LLM Implementation.

---
+++ frontend/src/generated/models/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnimationComponent';
+export * from './AnimationComponentAllOf';
+export * from './AnimationComponentAllOfParams';
+export * from './ChartComponent';
+export * from './ChartComponentAllOf';
+export * from './ChartDataPoint';
+export * from './ChartDataset';
+export * from './LayoutUpdate';
+export * from './TextComponent';
+export * from './TextComponentAllOf';
+export * from './UIComponent';
